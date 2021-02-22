db76b7157e659da6f3d4c1c06a907fc90bf52467
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2021-02-22 11:34:57 -0700

    Update format.py

--- python_for_sae/format.py
+++ python_for_sae/format.py
@@ -12,5 +12,9 @@ t.format(3,4)
 '{last}, {first}'.format(first = 'Scott', last = 'Warnock')
 print('[{x:5.0f}] [{x:5.1f}] [{x:5.2f}]'.format(x=12.3456))
 
+breakfast = "Breakfast today is: {}, {}, {}, and {}"
+
+
+breakfast.format('Juice', 'eggs', 'spam', 'bacon')
 
 https://pyformat.info/
