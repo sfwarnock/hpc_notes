2e31ad6ea7951331b8325338f2ff3935d1885f19
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2021-02-22 11:14:00 -0700

    Create format.py

---
+++ python_for_sae/format.py
@@ -0,0 +1,16 @@
+t = "first{}, second{}"
+t.format(3,4)
+
+t = "first {}, second {}"
+
+t = "first {0}, second {1}"
+t.format(3,4)
+
+t = "first {0}, second {1}, and second again {1}"
+t.format(3,4)
+
+'{last}, {first}'.format(first = 'Scott', last = 'Warnock')
+print('[{x:5.0f}] [{x:5.1f}] [{x:5.2f}]'.format(x=12.3456))
+
+
+https://pyformat.info/
