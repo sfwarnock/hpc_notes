fce37d5ac489ba110d22286c60d7745ff316ba64
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2021-03-04 05:34:22 -0700

    Add files via upload

---
+++ c/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  helloworld_c
+//
+//  Created by Warnock, Scott A on 3/4/21.
+//  Copyright © 2021 Warnock, Scott A. All rights reserved.
+//
+
+#include <stdio.h>
+
+main() {
+    printf("Hello, World!\n");
+}
