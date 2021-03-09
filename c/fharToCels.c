0b9fb49ad99bdc0d49265cb26887a7587350c2c4
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2021-03-09 08:43:51 -0700

    Add files via upload

---
+++ c/fharToCels.c
@@ -0,0 +1,26 @@
+//
+//  fharToCels.c
+//  fahr_to_cels
+//
+//  Created by Warnock, Scott A on 3/9/21.
+//  Copyright © 2021 Warnock, Scott A. All rights reserved.
+//
+
+#include <stdio.h>
+
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = lower;
+    while ( fahr <= upper){
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t%3.2f\n", fahr, celsius);
+        fahr += step;
+    }
+}
