fed815afc7dc26ccc23c3ecf32e502d3fd03a98a
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2021-03-10 08:24:23 -0700

    Add files via upload

--- c/main.c
+++ c/main.c
@@ -1,13 +1,27 @@
 //
 //  main.c
-//  helloworld_c
+//  cprog-e1.4
 //
-//  Created by Warnock, Scott A on 3/4/21.
+//  Created by Warnock, Scott A on 3/10/21.
 //  Copyright © 2021 Warnock, Scott A. All rights reserved.
 //
 
 #include <stdio.h>
 
-main() {
-    printf("Hello, World!\n");
+main()
+{
+    float celsius, fahr;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    celsius = lower;
+    printf("Celsius\tFahrenheit\n");
+    while (celsius <= upper){
+        fahr = celsius * (9.0 / 5.0) + 32;
+        printf("%3.0f\t\t\t%3.2f\n", celsius, fahr);
+        celsius += step;
+    }
 }
