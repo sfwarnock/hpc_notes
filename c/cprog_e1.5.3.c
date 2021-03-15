7b608401a65f1ed315b3702293afe18aa12395d1
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2021-03-15 15:23:20 -0600

    Add files via upload

---
+++ c/cprog_e1.5.3.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  cprog_e1.5.3
+//
+//  Created by Warnock, Scott A on 3/15/21.
+//  Copyright © 2021 Warnock, Scott A. All rights reserved.
+//
+
+#include <stdio.h>
+
+main()
+{
+    int c;
+    
+    while ((c = getchar()) != EOF)
+        putchar(c);
+}
